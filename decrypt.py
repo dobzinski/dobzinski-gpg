bea9df67083bdd201767d9628010cdf44ea9827c
Robson Dobzinski <robson.dobzinski@gmail.com>
2024-03-12 16:55:58 -0300

    Create decrypt.py

---
+++ decrypt.py
@@ -0,0 +1,18 @@
+from main import *
+
+def crypto(path):
+  directory = os.fsencode(path)
+  for file in os.listdir(directory):
+    d = os.fsdecode(directory)
+    filename = os.fsdecode(file)
+    dirFile = str(d) + str(filename)
+    if os.path.isfile(dirFile):
+      if (filename.lower().endswith('.gpg') == True):
+        subprocess.run(["./script/control.sh", "out", iniPassphrase, dirFile])
+    elif os.path.isdir(dirFile):
+        if (recursive == True):
+          crypto(dirFile+"/")
+
+for f in folders:
+  f = f.strip()
+  crypto(f)
